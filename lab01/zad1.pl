eb5b4d2e574399ec43acb8de536517f6ae29a10b
Wojciech <115220226+WojtexxD@users.noreply.github.com>
2025-02-26 12:15:40 +0100

    Add files via upload

---
+++ lab01/zad1.pl
@@ -0,0 +1,24 @@
+%Program: klocki_1
+%Baza wiedzy o uk³adzie klocków
+%definiowanie predykaty:
+%na/2
+%===============================
+
+
+%na(X,Y)
+%opis: spe³niony, gdy klocek X le¿y
+%bezpoœrednio na klocku Y
+%------------------------------
+  na(c,a).
+  na(c,b).
+  na(d,c).
+%------------------------------
+
+/*
+Informacje o budowie programu:
+Program sk³ada siê z 3 klauzul.
+Program zawiera 1 definicjê relacjii.
+Jest to relacja na/2.
+Definicja relacjii na/2 sk³ada siê z
+3 klauzul, które s¹ faktami.
+*/
